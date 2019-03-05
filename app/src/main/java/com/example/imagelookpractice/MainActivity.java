b95e9fc3e1cbfa32927925f0cc485176204c4be4
MuHongWeiWei <4a490094@stust.edu.tw>
2019-03-05 16:52:32 +0800

    設計cache,避免使用用戶的流量

--- app/src/main/java/com/example/imagelookpractice/MainActivity.java
+++ app/src/main/java/com/example/imagelookpractice/MainActivity.java
@@ -7,12 +7,16 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -57,39 +61,62 @@ public class MainActivity extends AppCompatActivity {
         new Thread() {
             @Override
             public void run() {
+                //[1]取得路徑
                 String path = etPath.getText().toString().trim();
-                try {
-                    URL url = new URL(path);
-                    HttpURLConnection coon = (HttpURLConnection) url.openConnection();
-                    coon.setRequestMethod("GET");
-                    coon.setConnectTimeout(5000);
-                    int code = coon.getResponseCode();
-                    if (code == 200) {
-                        InputStream is = coon.getInputStream();
-                        Bitmap bitmap = BitmapFactory.decodeStream(is);
+                //[2]緩存並且加密檔案
+                File file = new File(getCacheDir(), Base64.encodeToString(path.getBytes(), Base64.DEFAULT));
 
+                if (file.exists()) {
+                    //[3]從緩存裡取圖片
+                    System.out.println("使用緩存");
+                    Bitmap Cachebitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                    Message msg = Message.obtain();
+                    msg.obj = Cachebitmap;
+                    handler.sendMessage(msg);
+
+                } else {
+                    //[4]從網路上抓圖片
+                    try {
+                        System.out.println("第一次網路");
+                        URL url = new URL(path);
+                        HttpURLConnection coon = (HttpURLConnection) url.openConnection();
+                        coon.setRequestMethod("GET");
+                        coon.setConnectTimeout(5000);
+                        int code = coon.getResponseCode();
+                        if (code == 200) {
+                            InputStream is = coon.getInputStream();
+
+                            FileOutputStream fos = new FileOutputStream(file);
+
+                            int len = -1;
+                            byte[] buffer = new byte[1024];
+                            while ((len = is.read(buffer)) != -1) {
+                                fos.write(buffer, 0, len);
+                            }
+
+                            fos.close();
+                            is.close();
+
+                            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+
+                            Message msg = Message.obtain();
+                            msg.obj = bitmap;
+                            msg.what = REQUEST_SUCCED;
+                            handler.sendMessage(msg);
+                        } else {
+                            Message msg = Message.obtain();
+                            msg.what = REQUEST_FAIL;
+                            handler.sendMessage(msg);
+                        }
+                    } catch (Exception e) {
                         Message msg = Message.obtain();
-                        msg.obj = bitmap;
-                        msg.what = REQUEST_SUCCED;
+                        msg.what = REQUEST_EXCEPTION;
                         handler.sendMessage(msg);
-                    } else {
-                        Message msg = Message.obtain();
-                        msg.what = REQUEST_FAIL;
-                        handler.sendMessage(msg);
+                        e.printStackTrace();
                     }
 
-
-                } catch (Exception e) {
-                    Message msg = Message.obtain();
-                    msg.what = REQUEST_EXCEPTION;
-                    handler.sendMessage(msg);
-                    e.printStackTrace();
                 }
-
-
             }
         }.start();
-
-
     }
 }
